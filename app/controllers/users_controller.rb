01f3a5c51a9d0f95346abbbb8e9fd32ffa88475b
kieutrong <kieutrong91@gmail.com>
2017-06-08 14:18:39 +0700

    Chapter7_Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".no_created"
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+
+    unless @user
+      flash[:danger] = t(".note") << params[:id]
+      redirect_to signup_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   before_save {email.downcase!}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Setting.email.maximum},
+  validates :email, presence: true, length: {maximum: Settings.email.maximum},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :name, presence: true, length: {maximum: Setting.username.maximum}
-  validates :password, presence: true, length: {minimum: Setting.password.minimum}
+  validates :name, presence: true, length: {maximum: Settings.username.maximum}
+  validates :password, presence: true, length: {minimum: Settings.password.minimum}
 
   has_secure_password
 end
