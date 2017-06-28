4b3c106f72ccbfd2af16f6bcdfbdc099000d37a1
kieutrong <kieutrong91@gmail.com>
2017-06-28 09:37:49 +0700

    Add user following of Chapter 14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+
+  def render_404
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
 end
